a4917928a51ee30d94d03402ca1c8c2daa0da541
jasoncow <jasoncow@zillowgroup.com>
2023-04-03 21:18:17 -0700

    add out param 1 if added to the data structures, 0 if not

--- writefile.lua
+++ writefile.lua
@@ -9,4 +9,10 @@ if redis.call('HEXISTS', KEYS[1], ARGV[1]) == 0 then
 
     -- add to the processing list
     redis.call('RPUSH', KEYS[2], ARGV[1])
+
+    -- return 1 if added
+    return 1
 end
+
+-- return 0 if not added
+return 0
